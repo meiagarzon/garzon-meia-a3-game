6d23ad9bd64eefc27470264d855367c133ccf847
meiagarzon <000970924@mohawkcollege.ca>
2025-11-04 17:16:25 -0500

    Add background

--- garzon-meia-a3-game/Game.cs
+++ garzon-meia-a3-game/Game.cs
@@ -5,28 +5,21 @@ using System.Numerics;
 // The namespace your code is in.
 namespace MohawkGame2D
 {
-    /// <summary>
-    ///     Your game code goes inside this class!
-    /// </summary>
     public class Game
     {
         // Place your variables here:
-
-
-        /// <summary>
-        ///     Setup runs once before the game loop begins.
-        /// </summary>
+        Color turquoise = new Color ("#6d8d8a");
+         
         public void Setup()
         {
-
+            Window.SetSize(800, 600);
+            Window.SetTitle("Fish Game");
+            Window.ClearBackground(turquoise);
         }
 
-        /// <summary>
-        ///     Update runs every frame.
-        /// </summary>
         public void Update()
         {
-
+            
         }
     }
 
